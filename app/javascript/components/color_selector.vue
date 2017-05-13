9c0d9618f8c04953832ec1200aa784e8292a1b2e
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-05-13 21:17:45 +0900

    [vue] color_selector.vue

--- app/javascript/components/color_selector.vue
+++ app/javascript/components/color_selector.vue
@@ -1,14 +1,35 @@
 <template>
-  <div>Hello</div>
+  <div>
+    <div class="box red" @click="color = 'red'"
+      :class="{ selected: color === 'red' }">赤</div>
+    <div class="box blue" @click="color = 'blue'"
+      :class="{ selected: color === 'blue' }">青</div>
+    <div class="box green" @click="color = 'green'"
+      :class="{ selected: color === 'green' }">緑</div>
+    <div>color = "{{color}}"</div>
+  </div>
 </template>
 
 <script>
 module.exports = {
   data: function () {
-    return {}
+    return {
+      color: ""
+    }
   }
 }
 </script>
 
 <style scoped>
+  .box {
+    display: inline-block;
+    padding: 1rem;
+    color: white;
+    cursor: pointer;
+    border: 3px solid white;
+  }
+  .red { background-color: #c00 }
+  .blue { background-color: #00f }
+  .green { background-color: #080 }
+  .selected { border-color: black }
 </style>
