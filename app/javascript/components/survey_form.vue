3ee3e43db0281a5ec023cc2c4d79178cd37fc0ab
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-05-14 18:26:27 +0900

    [survey1] Vue.js 化

--- app/javascript/components/survey_form.vue
+++ app/javascript/components/survey_form.vue
@@ -8,25 +8,25 @@
           v-model="user.name">
       </div>
       <div class="form-group">
-        <label for="user-name">もっとも得意なプログラミング言語</label>
+        <label for="user-language">もっとも得意なプログラミング言語</label>
         <div>
           <label>
-            <input type="radio" name="user[language]" id="user-language"
+            <input type="radio" name="user[language]"
               v-model="user.language" value="ruby"> Ruby
           </label>
           <label>
-            <input type="radio" name="user[language]" id="user-language"
+            <input type="radio" name="user[language]"
               v-model="user.language" value="php"> PHP
           </label>
           <label>
-            <input type="radio" name="user[language]" id="user-language"
+            <input type="radio" name="user[language]"
               v-model="user.language" value="other"> その他
           </label>
         </div>
       </div>
-      <div class="form-group" v-show="user.language === 'other'">
+      <div class="form-group" v-show="user.language === 'other'" v-cloak>
         <label for="user-other-language">（具体的に）</label>
-        <input type="text" name="user[other_language]" id="user-language"
+        <input type="text" name="user[other_language]" id="user-other-language"
           style="display: inline; width: auto"
           class="form-control" v-model="user.other_language">
       </div>
